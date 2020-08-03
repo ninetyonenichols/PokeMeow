e01419486c08b9dd025b82ccf5ad57d34310793b
clmclean <mcharlie@email.arizona.edu>
2020-08-03 00:01:54 -0700

    finished style and layout of the gamepage

---
+++ public_html/home.js
@@ -0,0 +1,8 @@
+/* Filename: home.js
+ * Authors: Justin Nichols (jdnscience), Charles McLean (mcharlie)
+ * Class: CSc 337 Summer 2020
+ * Description: 
+ */
+
+//constants
+const serverURL = 'http://157.245.236.86';
