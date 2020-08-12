08838bb2671f302b2cedfea32b35361e1c5435bb
Justin Nichols <ninetyonenichols@gmail.com>
2020-08-12 07:33:10 +0000

    Added window system (main window, encounter window, and battle window). Still working on filling these windows w content.

--- public_html/home.js
+++ public_html/home.js
@@ -1,5 +1,5 @@
 /* Filename: home.js
- * Authors: Justin Nichols (jdnscience), Charles McLean (mcharlie)
+ * Authors: Justin Nichols (jdnscieArea.), Charles McLean (mcharlie)
  * Class: CSc 337 Summer 2020
  * Description:
  */
@@ -9,16 +9,17 @@ const serverURL = 'http://157.245.236.86';
 //const serverURL = 'http://64.227.49.233';
 var modeURL = '/command/';
 
+createWindows();
+
 $(document).ready(() => {
+    outs = $('#outputSection');
     printMain();
 });
 
 // Submit the command when either the button is clicked or 'Enter' is pressed
 $('#commandBtn').click(() => { submitCommand(); });
 $('#command').keypress(function (e) {
-    if (e.which == 13) {
-        submitCommand();
-    }
+    if (e.which == 13) { submitCommand(); }
 });
 
 /*  Description: This function submits the command string given by the user to
@@ -27,15 +28,13 @@ $('#command').keypress(function (e) {
  */
 function submitCommand() {
     //if no input, do nothing
-    const standoutStr = $('#command').val();
-    if (standoutStr == '') {
-        return;
-    }
+    const commandStr = $('#command').val();
+    if (commandStr == '') { return; }
 
     $.ajax({
         type: 'POST',
         url: serverURL + modeURL,
-        data: JSON.stringify({command: standoutStr}),
+        data: JSON.stringify({command: commandStr}),
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: (response) => {
@@ -43,13 +42,13 @@ function submitCommand() {
                 printMain();
                 modeURL = '/command/';
             } else if (response.encounter) {
-                printPkmn(response.encounter);
+                printEncounter(response.encounter);
                 modeURL = '/command/rand-enc/';
             } else if (response.battle) {
-                printOutput(response.battle);
+                printBattle(response.battle);
                 modeURL = '/command/battle/';
             } else {
-                printMsg('Invalid Command');
+                msg.text('Invalid Command');
             }
         }
     });
@@ -58,72 +57,97 @@ function submitCommand() {
     $('#command').val("");
 }
 
-
-/*  Description: This function puts a message in the message box
- *  Parameters:
- *      msg - the message to be displayed
+/* Creates the output windows and populates them with elements
  */
-function printMsg(msg) {
-    $('#msg').empty();
-    $('#msg').append(msg);
-}
+function createWindows() {
+    // Creating windows (main, eArea.unter, battle)
+    mw = $('<div>', { "id":"mw", "class":"outputWindow" });
+    ew = $('<div>', { "id":"ew", "class":"outputWindow" });
+    bw = $('<div>', { "id":"bw", "class":"outputWindow" });
 
-/* Description: clears the output window
- */
-function clow() {
-    $('#msg').empty();
-    $('#entity').empty();
+    // Creating messages to populate windows
+    msg = $('<div>', { "id":"msg" }); 
+
+    // Creating areas for pokemon / trainers to appear
+    mArea = $('<div>', { "id":"mArea", "class":"xArea" }); 
+    eArea = $('<div>', { "id":"eArea", "class":"xArea" }); 
+    bAreaL = $('<div>', { "id":"bAreaL", "class":"xArea" }); 
+    bAreaR = $('<div>', { "id":"bAreaR", "class":"xArea" }); 
+
+    // Adding pokemon/trainer areas
+    mw.append(mArea);
+    ew.append(eArea);
+    bw.append(bAreaL);
+    bw.append(bAreaR);
 }
 
 /* Description: This function prints out the options available to the player at
  *     the main game-screen.
  */
 function printMain() {
-    clow();
-    let bgUrl='./img/bg/city.png'
-    $('#outputWindow').css('background-image', `url(${bgUrl})`);
-    $('#msg').text('Options');
-    $('#entity').append(`random-encounter - starts an encounter
+    // Clearing previous content
+    outs.empty();
+    mArea.empty();
+
+    // Adding content to DOM elements
+    msg.text('Options');
+    mArea.append(`random-encounter - starts an encounter
         with a random pokemon<br>`);
-    $('#entity').append(`view-party - prints out a list of pokemon
+    mArea.append(`view-party - prints out a list of pokemon
         in your party<br>`);
-    $('#entity').append(`<view-pokemon - prints out a list of the
+    mArea.append(`<view-pokemon - prints out a list of the
         pokemon you\'ve caught<br>`);
-    $('#entity').append(`view name - prints out more information
+    mArea.append(`view name - prints out more information
         about pokemon \'name\'<br>`);
-    $('#entity').append(`remove name - removes pokemon \'name\'
+    mArea.append(`remove name - removes pokemon \'name\'
         from your party<br>`);
-    $('#entity').append(`add name - adds pokemon \'name\' to your
+    mArea.append(`add name - adds pokemon \'name\' to your
         party<br>`);
-    $('#entity').append(`release name - releases the pokemon
+    mArea.append(`release name - releases the pokemon
         \'name\' back to the wild<br>`);
+    
+    // Adding DOM elements to page
+    mw.prepend(msg);
+    outs.append(mw); 
 }
 
 /* Description: This function prints out the info for one pokemon
  * Parameters:
  *     pkmn - the object containing the pokemon's info
  */
-function printPkmn(pkmn) {
-    clow();
-    let bgUrl = './img/bg/kanto.png';
-    $('#outputWindow').css("background-image", `url(${bgUrl})`);
-    $('#msg').text(`A wild ${pkmn.name} appeared!`);
-    $('#entity').append(`${pkmn.name}<br>`);
-    $('#entity').append($('<img>', { src: pkmn.sprite, width: '160px',
+function printEncounter(pkmn) {
+    // Clearing previous content
+    outs.empty();
+    eArea.empty();
+
+    // Adding content to DOM elements
+    msg.text(`A wild ${pkmn.name} appeared!`);
+    eArea.append(`${pkmn.name}<br>`);
+    eArea.append($('<img>', { src: pkmn.sprite, width: '160px',
         alt: `A picture of ${pkmn.name}.` }));
-    $('#entity').append('<br>');
+    eArea.append('<br>');
     let type2 = pkmn.pType2 ? ` / ${pkmn.pType2}<br>` : '<br>';
-    $('#entity').append(`Type: ${pkmn.pType1}${type2}`);
+    eArea.append(`Type: ${pkmn.pType1}${type2}`);
+    
+    // Appending DOM elements to page
+    ew.prepend(msg);
+    outs.append(ew);
 }
 
-function printOutput(output) {
+function printBattle(output) {
+    // Clearing previous content
+    outs.empty();
+    bAreaL.empty();
+    bAreaR.empty();
+
+    outs.append(bw);
     if (output.trainer1) {
         const user = output.trainer1;
         const userPoke = user.party[user.active];
         const ai = output.trainer2;
         const aiPoke = ai.party[ai.active];
 
-        $('#entity').html(ai.name
+        eArea.html(ai.name
             + '<br>'
             + aiPoke.name + ' ' + aiPoke.currHp + '/' + aiPoke.maxHp
             + '<br><br>'
@@ -133,6 +157,6 @@ function printOutput(output) {
             + '<br>Moves: ' + userPoke.moves[0] + ', ' + userPoke.moves[1]);
 
     } else {
-        $('#entity').text(output);
+        bArea.text(output);
     }
 }
